21e8a69a754eccf472dd74f9d53e1f890d00618b
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-07-24 14:44:18 -0500

    New React app

---
+++ src/Greeting.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Greeting.js
+function Greeting({ language }) {
+  switch (language) {
+    case "es":
+      return <h1>Hola!</h1>;
+    case "fr":
+      return <h1>Bonjour!</h1>;
+    case "en":
+    default:
+      return <h1>Hello!</h1>;
+  }
+}
+export default Greeting;
